2366da2a592dacf53e79ae4f4b9fb131435903ea
Miguel-Orellana27 <orellana_miguel1@icloud.com>
2026-04-12 10:35:01 -0500

    feat:"random food placement and utilitarios.js was used"

--- cazando.js
+++ cazando.js
@@ -24,7 +24,6 @@ function graficarRectangulo(x, y, ancho, alto, color) {
 }
 
 function graficarGato() {
-    
     if (gatoX === 0 && gatoY === 0) {
         gatoX = (canvas.width / 2) - (anchoGato / 2);
         gatoY = (canvas.height / 2) - (altoGato / 2);
@@ -49,16 +48,28 @@ function actualizarDibujo() {
     graficarComida();   
     graficarGato();    
 }
+
+
 function detectarColision() {
     if (gatoX + anchoGato > comidaX &&
         gatoX < comidaX + anchoComida &&
         gatoY + altoGato > comidaY &&
         gatoY < comidaY + altoComida) {
         
-        alert("¡Colisión! ¡Has tocado la comida!");
+       
+       
+        comidaX = generarAleatorio(0, canvas.width - anchoComida);
+        comidaY = generarAleatorio(0, canvas.height - altoComida);
+        
+       
+        actualizarDibujo(); 
 
+        
+        puntos++; 
+        mostrarEnSpan("puntos", puntos); 
     }
 }
+
 
 
 function moverIzquierda() {
@@ -72,7 +83,6 @@ function moverIzquierda() {
 
 function moverDerecha() {
     gatoX += velocidadMovimiento;
-    
     if (gatoX + anchoGato > canvas.width) {
         gatoX = canvas.width - anchoGato; 
     }
@@ -82,7 +92,6 @@ function moverDerecha() {
 
 function moverArriba() {
     gatoY -= velocidadMovimiento;
-   
     if (gatoY < 0) {
         gatoY = 0; 
     }
@@ -92,7 +101,6 @@ function moverArriba() {
 
 function moverAbajo() {
     gatoY += velocidadMovimiento;
-    
     if (gatoY + altoGato > canvas.height) {
         gatoY = canvas.height - altoGato; 
     }
@@ -104,27 +112,26 @@ function moverAbajo() {
 
 
 function iniciarJuego() {
-    
+  
     graficarGato();
 
     
-    comidaX = Math.random() * (canvas.width - anchoComida);
-    comidaY = Math.random() * (canvas.height - altoComida);
-    
-    if (comidaX < 0) comidaX = 0;
-    if (comidaY < 0) comidaY = 0;
-    if (comidaX + anchoComida > canvas.width) comidaX = canvas.width - anchoComida;
-    if (comidaY + altoComida > canvas.height) comidaY = canvas.height - altoComida;
-
-    graficarComida(); 
+    comidaX = generarAleatorio(0, canvas.width - anchoComida);
+    comidaY = generarAleatorio(0, canvas.height - altoComida);
 
     
+    graficarComida();
+
+    
+    puntos = 0;
+    mostrarEnSpan("puntos", puntos); 
+
+
     let btnIzquierda = document.getElementById("btnIzquierda");
     let btnDerecha = document.getElementById("btnDerecha");
     let btnArriba = document.getElementById("btnArriba");
     let btnAbajo = document.getElementById("btnAbajo");
 
-    
     if (!btnIzquierda) {
         console.error("Error crítico: Botón 'btnIzquierda' no encontrado. No se puede continuar.");
         return;
@@ -149,6 +156,5 @@ function iniciarJuego() {
     }
     btnAbajo.addEventListener('click', moverAbajo);
 
-    
     console.log("Juego iniciado correctamente. Controles listos.");
 }
