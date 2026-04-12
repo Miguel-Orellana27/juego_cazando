38a7f48297d4ef62a1c92f2d2ac7b24b9da6488f
Miguel-Orellana27 <orellana_miguel1@icloud.com>
2026-04-12 09:11:37 -0500

    feat:"Implement left movement for cat and canvas clearing"

--- cazando.js
+++ cazando.js
@@ -14,13 +14,8 @@ const altoComida = 20;
 const anchoComida = 20;  
  
 function graficarGato() {
-   
- 
-    // Calculamos el centro exacto del canvas
-    // (Ancho del canvas / 2) - (Ancho del objeto / 2)
     gatoX = (canvas.width / 2) - (anchoGato / 2);
     gatoY = (canvas.height / 2) - (altoGato / 2);
-    //color del gato
     let colorG= "#1900ff";
     graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, colorG);
 }
@@ -29,15 +24,48 @@ function graficarComida() {
     graficarRectangulo(comidaX,comidaY,anchoComida, altoComida, "#FF0808");
 }
  
-function iniciarJuego() {
-    // Llamamos a las funciones que ya creaste
-    graficarGato();
-    graficarComida();
-}
- 
 function graficarRectangulo(x,y,ancho,alto,color){
     ctx.fillStyle = color;
     ctx.fillRect(x, y, ancho, alto);
- 
+}
+
+
+const velocidadMovimiento = 10;
+
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function moverIzquierda() {
+    gatoX -= velocidadMovimiento;
+
+    if (gatoX < 0) {
+        gatoX = 0;
+    }
+
+    limpiarCanvas();      
+    dibujarComida();      
+    dibujarGato();        
+}
+
+let btnIzquierda = document.getElementById("btnIzquierda");
+
+function iniciarJuego() {
+    graficarGato(); 
+
+    comidaX = Math.random() * (canvas.width - anchoComida);
+    comidaY = Math.random() * (canvas.height - altoComida);
+    if (comidaX < 0) comidaX = 0;
+    if (comidaY < 0) comidaY = 0;
+    if (comidaX + anchoComida > canvas.width) comidaX = canvas.width - anchoComida;
+    if (comidaY + altoComida > canvas.height) comidaY = canvas.height - altoComida;
+    
+    graficarComida(); 
+
+    if (btnIzquierda) { 
+      btnIzquierda.addEventListener('click', moverIzquierda);
+    } else {
+      console.error();
+    }
 }
 
